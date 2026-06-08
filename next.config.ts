854566606af1c8c7147a3e410665ddeef175fbb9
Aurora Scharff <aurora.sofie@gmail.com>
2026-06-08 16:22:56 +0200

    Update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   cacheComponents: true,
   reactCompiler: true,
+  partialPrefetching: true,
   experimental: {
     inlineCss: true,
     appShells: true,
