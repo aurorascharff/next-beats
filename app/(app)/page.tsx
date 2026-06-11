88fa847c37699b2688d727aa2a4df6f1b3967886
Aurora Scharff <aurora.sofie@gmail.com>
2026-06-11 13:56:29 +0200

    fix home for starter

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,26 +1,5 @@
 import { PageWrapper } from '@/components/ui/page-layout';
-import { TopGenresGrid } from '@/features/genre/components/genre-browse';
-import { PlaylistBrowse } from '@/features/playlist/components/playlist-browse';
-import { MostPlayed } from '@/features/track/components/most-played';
-import { QuickPlayGrid } from '@/features/track/components/quick-play-grid';
 
 export default function HomePage() {
-  return (
-    <>
-      <PageWrapper title="Welcome back">
-        <h2 className="mb-4">Recently Played</h2>
-        <QuickPlayGrid />
-        <h2 className="mt-10 mb-4">Most Played</h2>
-        <MostPlayed />
-        <section className="mt-10">
-          <h2 className="mb-4">Your Playlists</h2>
-          <PlaylistBrowse />
-        </section>
-        <section className="mt-10">
-          <h2 className="mb-4">Browse Genres</h2>
-          <TopGenresGrid />
-        </section>
-      </PageWrapper>
-    </>
-  );
+  return <PageWrapper title="Welcome back" />;
 }
