a384ae315fa740b62cf89f31d6e1e9d9bae04f3c
Aurora Scharff <aurora.sofie@gmail.com>
2026-06-11 13:58:13 +0200

    Update

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,5 +1,12 @@
 import { PageWrapper } from '@/components/ui/page-layout';
 
 export default function HomePage() {
-  return <PageWrapper title="Welcome back" />;
+  return (
+    <PageWrapper title="Welcome back">
+      {/* Recently Played */}
+      {/* Most Played */}
+      {/* Your Playlists */}
+      {/* Browse Genres */}
+    </PageWrapper>
+  );
 }
--- app/(app)/playlist/[id]/page.tsx
+++ app/(app)/playlist/[id]/page.tsx
@@ -1,5 +1,10 @@
 import { PageWrapper } from '@/components/ui/page-layout';
 
 export default function PlaylistDetailPage(_props: PageProps<'/playlist/[id]'>) {
-  return <PageWrapper>Playlist</PageWrapper>;
+  return (
+    <PageWrapper>
+      {/* Playlist detail */}
+      {/* Other Playlists */}
+    </PageWrapper>
+  );
 }
