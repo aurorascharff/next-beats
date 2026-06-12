4c4901b6f0f55a94f2a5e8198ef68e5102a5b207
Aurora Scharff <aurora.sofie@gmail.com>
2026-06-12 06:58:11 +0200

    fix autofill playlsit form

--- features/playlist/components/create-playlist-form.tsx
+++ features/playlist/components/create-playlist-form.tsx
@@ -23,6 +23,9 @@ export function CreatePlaylistForm() {
           placeholder="New playlist name…"
           required
           autoComplete="off"
+          data-1p-ignore
+          data-lpignore="true"
+          data-form-type="other"
           className="flex-1"
           onKeyDown={e => {
             if (e.key === 'Enter' && e.metaKey) {
